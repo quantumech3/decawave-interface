87aeabae28299337f3dfa959a7d529b624b97314
quantumech3 <quantumech3@gmail.com>
2020-11-29 18:54:40 -0500

    Setup project stucture (excluding android project)

---
+++ extract_library.bat
@@ -0,0 +1,5 @@
+rm -r .\out\library
+cp -r .\src\unity\DecawaveInterface\Assets\Plugins .\out\library
+
+echo "To use the library, drag the contents of ./out/library/Plugins into the plugins folder in your unity project"
+pause
