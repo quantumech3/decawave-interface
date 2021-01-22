312c6c7619e10ccbbcef2d135eb28937a606f6ec
quantumech3 <quantumech3@gmail.com>
2021-01-22 01:16:52 -0500

    Made testing environment for High Level Interface

---
+++ src/unity/DecawaveInterface/Assets/Tests/HighLevel/NetworkGUI/Scripts/NetworkGUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkGUI : MonoBehaviour
+{
+    NetworkManager networkManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    public void StartServer()
+    {
+        networkManager.StartServer();
+    }
+
+    public void StartClient()
+    {
+        networkManager.StartClient();
+    }
+
+    public void SetAddress(string addr)
+    {
+        networkManager.networkAddress = addr;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
